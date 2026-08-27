f8c8ab50cbbb808d474010ff37be56a1217a8a18
ashish singh <ashishsingh094562@gmail.com>
2026-08-27 21:55:20 +0530

    commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://airbnb-room-type-predictor-bvjh.onrender.com";
 
 const form = document.getElementById("predictionForm");
 const predictButton = document.getElementById("predictButton");
